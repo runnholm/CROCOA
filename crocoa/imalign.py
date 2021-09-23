dee09dfd5adbb4fcc0cc83a007a575248de10fb5
Axel Runnholm <axel.runnholm@astro.su.se>
2021-09-23 11:33:46 +0200

    Fix devnull undefined

--- crocoa/imalign.py
+++ crocoa/imalign.py
@@ -257,6 +257,6 @@ def get_image_coords(header):
 @contextmanager
 def suppress_stdout_stderr():
     """A context manager that redirects stdout and stderr to devnull"""
-    with open(devnull, 'w') as fnull:
+    with open(os.devnull, 'w') as fnull:
         with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
             yield (err, out)
