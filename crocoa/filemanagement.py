7840bf3e91813d441cec6ccf11f26590c806f524
Axel Runnholm <axel.runnholm@astro.su.se>
2023-05-11 11:50:08 +0200

    start adding a class based file management system

--- crocoa/filemanagement.py
+++ crocoa/filemanagement.py
@@ -2,12 +2,13 @@ import glob
 import os
 import shutil
 from astropy.io import fits
+
+
 def make_copy(source_files, destination_dir, verbose=False):
-    """ Function for making a clean copy
-    """
+    """Function for making a clean copy"""
     if verbose:
-        print('Copying', source_files)
-        print('Destination', destination_dir)
+        print("Copying", source_files)
+        print("Destination", destination_dir)
 
     filelist = []
     for fn in source_files:
@@ -22,13 +23,13 @@ def make_copy(source_files, destination_dir, verbose=False):
 
     return filelist
 
+
 def cleanup(directory):
     shutil.rmtree(directory)
 
 
-
 def write_wcs_to_fits(input_image, dra, ddec):
-    """ Writes the new coordinates into all the headers in the fits image.
+    """Writes the new coordinates into all the headers in the fits image.
 
     Parameters
     ----------
@@ -44,8 +45,11 @@ def write_wcs_to_fits(input_image, dra, ddec):
     -------
     None
     """
-    print('Propagating changes to: ', input_image,)
-    print('Which has the following hdu objects')
+    print(
+        "Propagating changes to: ",
+        input_image,
+    )
+    print("Which has the following hdu objects")
     with fits.open(input_image, mode="update") as hdul:
         print(hdul)
         for hdu in hdul:
@@ -63,3 +67,37 @@ def write_wcs_to_fits(input_image, dra, ddec):
 
         hdul.flush()
 
+
+class Image:
+    def __init__(self, filename, verbose=False) -> None:
+        self.original = filename
+        self.verbose = verbose
+    
+    def make_working_copy(self, destination_dir):
+        filelist = make_copy([self.original], destination_dir, verbose=self.verbose)
+        self.working_copy = filelist[0]
+    
+    def backpropagate_wcs(self, dra, ddec):
+        write_wcs_to_fits(self.original, dra, ddec)
+    
+
+class ImageSet:
+    def __init__(self, file_list, drizzle_config) -> None:
+        self.images = [Image(file_name) for file_name in file_list]
+        self.drizz_result = None
+        self.drizz_config = drizzle_config
+
+
+    def drizzle(self):
+        pass
+
+
+def align_multiple_filters(image_sets, reference_set_index=0):
+    for image_set in image_sets:
+        image_set.drizzle()
+    
+    
+
+
+
+    
