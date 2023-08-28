ee861977af53b0c5f802a92d203b38948ffa5d82
Axel Runnholm <axel.runnholm@astro.su.se>
2023-08-28 12:21:03 +0200

    put suffix in correct place with respect to flt and flc

--- crocoa/filemanagement.py
+++ crocoa/filemanagement.py
@@ -84,7 +84,13 @@ class Image:
         if file_suffix is None:
             self.name = basename
         else:
-            self.name = basename.split(".")[0] + file_suffix + basename.split(".")[1]
+            # put suffix on the right side of flt
+            if 'flt' in basename:
+                self.name = basename.split("flt")[0] + file_suffix + '_flt' + basename.split(".")[1]
+            if 'flc' in basename:
+                self.name = basename.split("flc")[0] + file_suffix + '_flc' + basename.split(".")[1]
+            else:
+                self.name = basename.split(".")[0] + file_suffix + basename.split(".")[1]
         self.original = filename
         self.verbose = verbose
         self.target_copy = None
