9f411e14053d1992747e02c6694c9eb3b776d02c
Axel Runnholm <axel.runnholm@astro.su.se>
2023-08-28 12:12:27 +0200

    stupid :

--- crocoa/filemanagement.py
+++ crocoa/filemanagement.py
@@ -19,7 +19,7 @@ def make_copy(source_files, destination_dir, verbose=False):
         name = os.path.basename(fn)
         if not os.path.isdir(destination_dir):
             os.mkdir(destination_dir)
-        if isinstance(destination_dir, Path)
+        if isinstance(destination_dir, Path):
             new_name = destination_dir / name
         else:
             new_name = destination_dir + name
